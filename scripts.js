35610a0cc69dd62cdcf2549612d21d81fb681f76
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-04-30 05:23:07 -0400

    change game logic, display results using DOM methods

--- scripts.js
+++ scripts.js
@@ -1,15 +1,16 @@
 const choices = ["ROCK","PAPER","SCISSORS"]
+const buttons = document.querySelectorAll("button")
+const results = document.querySelector(".results")
+const runningScore = document.querySelector(".runningScore")
+const instructions = document.querySelector(".instructions")
 
 function getComputerChoice(choices){
+    //computer choice is selected randomly from list of choices
     return choices[Math.floor(Math.random()*choices.length)]
 }
 
-function validatePlayerSelection(playerSelection){
-    return choices.includes(playerSelection)
-}
-
 function playRound(computerSelection,playerSelection){
-
+//paper beats rock, rock beats scissors, scissors beats paper
     if (computerSelection === playerSelection){
         return("It's a tie")
     }else{
@@ -25,6 +26,15 @@ function playRound(computerSelection,playerSelection){
         }
     }
 }
+
+function updateScore(result,computerSelection,playerSelection){
+    if (result===`You lose! ${computerSelection} beats ${playerSelection}`){
+        computerScore++
+    }else if (result === `You win! ${playerSelection} beats ${computerSelection}`){
+        playerScore++
+    }
+}
+
 function declareWinner (computerScore,playerScore){
     if (computerScore>playerScore){
         return(`The computer wins with a score of ${computerScore}:${playerScore}`)
@@ -35,31 +45,27 @@ function declareWinner (computerScore,playerScore){
     }
 }
 
-function game(){
-    let computerScore = 0
-    let playerScore = 0
 
-    for (let i=1; i<=5; i++){
-        const computerSelection = getComputerChoice(choices).toUpperCase()
-        const playerSelection = prompt("Rock,Paper or Scissors?:").toUpperCase()
+let computerScore = 0
+let playerScore = 0
+let result
 
-        if (validatePlayerSelection(playerSelection)){
-            result=playRound(computerSelection,playerSelection)
-            console.log(result)
+buttons.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        if (computerScore<5 && playerScore<5){
 
-            if (result===`You lose! ${computerSelection} beats ${playerSelection}`){
-                computerScore++
-            }else if (result === `You win! ${playerSelection} beats ${computerSelection}`){
-                playerScore++
-            }
+            //get computer choice and player choice
+            const computerSelection=getComputerChoice(choices).toUpperCase()
+            const playerSelection = button.textContent.toUpperCase()
+            //get winner of round and display on DOM
+            result= playRound(computerSelection,playerSelection)
+            results.textContent=result
+            //update scores and display on DOM
+            updateScore(result,computerSelection,playerSelection)
+            runningScore.textContent = `Computer: ${computerScore}:Player: ${playerScore}`
+
         }else{
-            console.log(
-        `Invalid choice:`)
+            instructions.textContent = declareWinner(computerScore,playerScore)
         }
-    }
-    alert(declareWinner(computerScore,playerScore))
-}
-
-game()
-
-
+    })
+})
